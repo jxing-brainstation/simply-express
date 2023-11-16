01f6dbce34ba0d68cf09628f801a360355d6fcb7
jennyxing <jennyxing27@gmail.com>
2023-11-16 12:27:54 -0500

    make text red

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.get('/', (req, res) => {
-    res.send(`<h1 style="color:blue">Welcome to my BRAND NEW Express App</h1>`);
+    res.send(`<h1 style="color:red">Welcome to my BRAND NEW Express App</h1>`);
 });
 
 app.listen(PORT, () => {
